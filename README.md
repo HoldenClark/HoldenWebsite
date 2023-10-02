2236ef0e4b1df3355d68d54b7338edd1cae653b8
Holden Clark <holdenjclark@gmail.com>
2023-10-02 16:13:18 -0400

    Changes include:  Images have been updated/replaced  Blog feature has been added  'About Me' section has been added to  files have been reorganized

--- README.md
+++ README.md
@@ -1,3 +1,13 @@
 # HoldenWebsite
-basic website to showcase my portfolio
+This repository contains the basic implementation of my e-portfolio, designed to showcase various projects I’ve worked on. Currently, the website features a brief introduction about me, my resume, a blog feature, and a project named “StarCraft Game Counter”.
+
+However, it’s important to note that several projects are currently missing from the portfolio. This is either because they haven’t been implemented into the website yet, or the projects themselves are still incomplete.
+
+In the near future, I plan to add more features to the website. One such feature is a server-side database, which will be used to store blog posts.
+
+Additionally, I’m excited to share that I’ll be implementing more projects onto the website. One of these is the “Alien Invasion” project, which is a game created using Pygame. My goal is to fully develop this game and make it downloadable for anyone interested in playing it.
+
+Another project I’m eager to showcase is a demo of a bot I’ve created that can play the video game StarCraft 2 against other AI bots.
+
+Thank you for visiting my e-portfolio. Stay tuned for these exciting updates!
 https://holden-website.vercel.app/
--- index.js
+++ index.js
@@ -1,6 +1,14 @@
+//creates global variable dateOfToday which is the current date in real time and is used when creating a loading blog posts to provide an accurate time.
+var currentDate = new Date();
+let dateOfToday = currentDate.getFullYear() + "-" + (currentDate.getMonth() + 1) + "-" + (currentDate.getDay() + 1);
+var i = 0;
+
+//global variable declaring if user is allowed to post set to false initially.
+var access = false;
+
 function openTab(evt, tabName) {
     // Declare all variables
-    var i, tabcontent, tablinks;
+    var tabcontent, tablinks;
   
     // Get all elements with class="tabcontent" and hide them
     tabcontent = document.getElementsByClassName("tabcontent");
@@ -16,7 +24,108 @@ function openTab(evt, tabName) {
   
     // Show the current tab, and add an "active" class to the button that opened the tab
     document.getElementById(tabName).style.display = "block";
-    evt.currentTarget.className += " active";
   }
 
-window.onload(openTab("tab", 'About Me'))
+  //if local storage is empty an array is created to be used later so we can avoid null and undefined errors when dealing with local storage and initial inputs later. upon deployment this section of code will only one time until "delete posts" feature is added
+  if (localStorage.length === 0) {
+    var jsonArray = [{0 : "post"}];
+    localStorage.setItem("posts", JSON.stringify(jsonArray));
+  }
+  
+  //function that logs text submitted from text-area and turns it into html element 'blogpost' and appends it to the body of the blogs tab
+  function logValue() {
+    //sets what we want to post and have submitted to a variable
+    var textAreaValue = document.getElementById("posts").value;
+
+    //retrieves the current array from local storage
+    var retrievedData = localStorage.getItem("posts");
+    var retrievedObj = JSON.parse(retrievedData);
+    jsonArray = retrievedObj;
+
+    // get the 'posts' object from what we just retrieved
+    var keys = Object.keys(retrievedObj);
+
+    // Get the last key from the array and increment i for the next key
+    var lastKey = keys[keys.length - 1];
+
+    //this is to avoid some weird errors with lastkey being a string that we need to increment
+    lastKey = parseInt(lastKey);
+    lastKey++;
+    lastKey = lastKey.toString();
+
+    //i is the key in the json array we are storing the value in the key item pair will be the value from the text-area we have submitted (example: {1 : "New blog post!"}) where 1 is i that will be incremented and the text is the var textAreaValue
+    i = lastKey;
+    
+    //add new values to JSON array and set the new array to local storage replacing the old one with new array + 1 new item
+    if (access) {
+      jsonArray.push({[i] : textAreaValue});
+      localStorage.setItem("posts", JSON.stringify(jsonArray));
+
+      // Create a new html element plus styling for the post
+      var p = document.createElement("p");
+      p.style.border = '2px solid black';
+      p.style.borderRadius = '10px';
+      p.style.minHeight = '60px';
+      p.style.background = 'linear-gradient(120deg, white, #D7CDCC)';
+      p.innerHTML = dateOfToday + "<br>" + textAreaValue;
+
+      // Append the new element to the body of the blog tab
+      document.getElementById("blogpost").appendChild(p);
+    }
+  }
+
+  //upon hitting the blog tab this line triggers the function to load all previous posts stored in local storage
+  document.getElementById("blog").addEventListener("click", loadBlog());
+
+
+  //this function loads the blog upon clicking on the blog tab and takes all values from the jsonarray in local storage and creates and html element for each post stored.
+  function loadBlog() {
+    //this block of code is used to find the last key in the array in local storage. first we use the key "posts" to get the value that is our array. Once we have the array we find the keys and then finally we locate the lastKey and convert it to type int
+    var retrievedData = localStorage.getItem("posts");
+    var retrievedObj = JSON.parse(retrievedData);
+    var keys = Object.keys(retrievedObj);
+    var lastKey = keys[keys.length - 1];
+    lastKey = parseInt(lastKey);
+
+    // for each item post blogpost with key
+    // read each item from JSON array (each value)
+    if (localStorage.length != 0) {
+      var retrievedData = localStorage.getItem("posts");
+      var retrievedObj = JSON.parse(retrievedData);
+
+        //starts from 1 to avoid placeholder post item at index 0. goes until it has reached lastKey which is the last key in the array that stores our posts so that all posts are loaded.
+        for (let j = lastKey; j > 0; j--) {
+          var p = document.createElement("p");
+          p.style.border = '2px solid black';
+          p.style.borderRadius = '10px';
+          p.style.minHeight = '60px';
+          p.style.background = 'linear-gradient(120deg, white, #D7CDCC)';
+          p.innerHTML = dateOfToday + "<br>" + retrievedObj[j][j];
+          document.getElementById("blogpost").appendChild(p);
+        }
+      }
+    }
+
+    // function that removes the last post from the blog (password owners only)
+    function deleteLastPost() {
+      if (access) {
+        var retrievedData = localStorage.getItem("posts");
+        var retrievedObj = JSON.parse(retrievedData);
+        retrievedObj.pop();
+        localStorage.setItem("posts", JSON.stringify(retrievedObj));
+        location.reload();
+      }
+    }
+
+    //function that allows only users who have entered the right password to create a post
+    function updateAccess() {
+      //gets value entered from password form and compares it to set password
+      var enteredPassword = document.getElementById("pwd").value;
+      var password = "Oscar";
+        if (enteredPassword == password) {
+          access = true;
+        }
+    }
+  
+
+window.onload = openTab("tab", 'About Me');
